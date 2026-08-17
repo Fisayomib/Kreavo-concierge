37159a953c9c056caabe137507d9fac7fb3e3b8d
Fisayomi <fizzybabz456@gmail.com>
2026-08-17 12:41:50 +0100

    KC-002: branch on user input with if/elif/else to assign a grade

--- sprint1_order2.py
+++ sprint1_order2.py
@@ -11,4 +11,12 @@ print("Score:", score)
 print("Name type:", type(name))
 print("Score type:", type(score))
 
-
+grade = int(input("Enter your grade: "))
+if grade >= 90:
+    print("You got an A")
+elif grade >= 80:
+    print("You got a B")
+elif grade >= 70:
+    print("You got a C")
+else:
+    print("You got a D")
