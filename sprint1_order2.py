ef16604fc64e72476703e50d47074f2d88a1c6ec
Fisayomi <fizzybabz456@gmail.com>
2026-08-17 10:28:10 +0100

    Checks the type of two variables- name & score, and prints them out

--- sprint1_order2.py
+++ sprint1_order2.py
@@ -4,3 +4,11 @@ summ = a + b
 product = a * b
 print("Sum:", summ)
 print("Product:", product)
+name = "Fisayomi"
+score = 90.06
+print("Name:", name)
+print("Score:", score)
+print("Name type:", type(name))
+print("Score type:", type(score))
+
+
