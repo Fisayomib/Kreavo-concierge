1681c4533c71da10d9e54d2f571237412ff957a7
Fisayomi <fizzybabz456@gmail.com>
2026-08-19 12:14:00 +0100

    KC-003: rewrite README.md line count by hand, replacing an AI-suggested line in 8e1528e

--- sprint1_order3_3.py
+++ sprint1_order3_3.py
@@ -1,5 +1,5 @@
 total = 0
 with open("README.md") as file:
     for line in file:
-        total += len(line.split())
-print("Total number of words in README.md:", total)
+        total += 1
+print("Total number of lines in README.md:", total)
