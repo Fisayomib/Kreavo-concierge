52b470e5bbbe2ad0a6d1dc83bfb3d7b2166eda93
Fisayomi <fizzybabz456@gmail.com>
2026-08-24 13:37:47 +0100

    KC-005: Added a function to calculate total values/ages in the dictionary

--- sprint1_order3_2.py
+++ sprint1_order3_2.py
@@ -6,3 +6,12 @@ print("Total age of all individuals:", total)
 
 for name, age in name_age.items():
     print(f"{name} is {age} years old.")
+
+
+def summ(data):
+    tottal = 0
+    for i in data.values():
+        tottal += i
+    return tottal
+
+print(summ(name_age))
