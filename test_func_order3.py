7fae1867594f27f465b345672e18fa28d23ba57f
Fisayomi <fizzybabz456@gmail.com>
2026-08-24 15:23:03 +0100

    KC-006: the two asserts moved into a function called test_summ, so pytest can run

--- test_func_order3.py
+++ test_func_order3.py
@@ -2,5 +2,8 @@ from func_order3_2 import summ
 
 babz = {"Fisayo": 21, "Fisol": 19, "Fisope": 17}
 emp = {}
-assert summ(babz) == 57
-assert summ(emp) == 0
+
+
+def test_summ():
+    assert summ(babz) == 57
+    assert summ(emp) == 0
