49efbcb845bc3063bd75ead9fb80aa60360d8de2
Fisayomi <fizzybabz456@gmail.com>
2026-08-31 14:18:53 +0100

    SKEL-001: Made the ModuleNotFoundError print the required fix

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
-from flask import Flask 
+try:
+    from flask import Flask 
 
-app = Flask(__name__)
-if __name__ == "__main__":
-    app.run()
+
+    app = Flask(__name__)
+    if __name__ == "__main__":
+        app.run()
+except ModuleNotFoundError:
+    print("run pip install -r requirements.txt")
+
+
