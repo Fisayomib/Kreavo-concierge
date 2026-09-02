a4ae051f84165ae7dafbc19a2b1adf7914fbd06c
Fisayomi <fizzybabz456@gmail.com>
2026-09-02 11:28:07 +0100

    SKEL-002: Added a health route to the site, so a developer can see if it's healthy with it's version number

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
+VERSION = "0.1.0"
 try:
     from flask import Flask 
 
 
     app = Flask(__name__)
+    @app.route("/health")
+    def status():
+        check = {"status": "healthy", "version": VERSION}
+        return check
     if __name__ == "__main__":
         app.run()
 except ModuleNotFoundError:
     print("run pip install -r requirements.txt")
+
+
+
 
 
